4985902a1c3f436d92dca8eef7a52d3f2e30bf38
Pedro Monteagudo <pedromonteagudo@hotmail.com>
2020-08-20 17:58:24 +0000

    change ip by localhost

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const credentials =
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://85.208.22.12:27017/my_portfolio", { useNewUrlParser: true, useUnifiedTopology: true  })
+  .connect("mongodb://localhost:27017/my_portfolio", { useNewUrlParser: true, useUnifiedTopology: true  })
   .then(() => {
     console.log("Node connected to the DB !!");
 
